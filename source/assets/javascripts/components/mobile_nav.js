6aef37b92ebe3aec098a7125d69f4dbb78568fd7
Xelaflash <xelaflash@gmail.com>
2019-05-21 17:36:42 -0400

    js event listener not working to fix

--- source/assets/javascripts/components/mobile_nav.js
+++ source/assets/javascripts/components/mobile_nav.js
@@ -1,1 +1,13 @@
-console.log("hello from here components");
+const navBtn = document.querySelector(".burger");
+const closeBtn = document.querySelector(".closebtn");
+
+function openNav() {
+  document.querySelector(".sidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.querySelector(".sidenav").style.width = "0";
+}
+
+navBtn.addEventListener("click", openNav);
+closeBtn.addEventListener("click", closeNav);
--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -5,3 +5,23 @@ import '@fortawesome/fontawesome-free/js/all.min';
 
 // components JS files
 import './components/mobile_nav';
+
+console.log("hello here");
+
+const navBtn = document.querySelector(".burger");
+const closeBtn = document.querySelector(".closebtn");
+
+function openNav() {
+  console.log("clicked");
+  document.querySelector(".sidenav").style.width = "100%";
+}
+
+function closeNav() {
+    console.log("clicked");
+  document.querySelector(".sidenav").style.width = "0";
+}
+
+navBtn.addEventListener("click", openNav);
+closeBtn.addEventListener("click", closeNav);
+
+export { openNav, closeNav };
