6b1da505d918354188f460f58a05902335b34f54
Xelaflash <xelaflash@gmail.com>
2019-05-22 11:26:12 -0400

    remove test burger anim

--- source/assets/javascripts/components/mobile_nav.js
+++ source/assets/javascripts/components/mobile_nav.js
@@ -1,11 +1,9 @@
 function openNav() {
-  // document.querySelector(".sidenav").style.width = "100%";
   document.querySelector(".sidenav").style.width = "100%";
 }
 
 function closeNav() {
-  // document.querySelector(".sidenav").style.width = "0";
-    document.querySelector(".sidenav").style.width = "0";
+  document.querySelector(".sidenav").style.width = "0";
 }
 
 export { openNav, closeNav };
--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -4,7 +4,6 @@ import 'bootstrap';
 // components JS files
 import { openNav, closeNav } from './components/mobile_nav';
 
-
 const navBtn = document.querySelector(".burger");
 const closeBtn = document.querySelector(".closebtn");
 
