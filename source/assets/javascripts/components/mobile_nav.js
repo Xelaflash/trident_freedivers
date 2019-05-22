94c04775c07947fe278cfb646d647a6d8433f056
Xelaflash <xelaflash@gmail.com>
2019-05-22 11:07:57 -0400

    ide nav style for phone

--- source/assets/javascripts/components/mobile_nav.js
+++ source/assets/javascripts/components/mobile_nav.js
@@ -1,9 +1,11 @@
 function openNav() {
+  // document.querySelector(".sidenav").style.width = "100%";
   document.querySelector(".sidenav").style.width = "100%";
 }
 
 function closeNav() {
-  document.querySelector(".sidenav").style.width = "0";
+  // document.querySelector(".sidenav").style.width = "0";
+    document.querySelector(".sidenav").style.width = "0";
 }
 
 export { openNav, closeNav };
