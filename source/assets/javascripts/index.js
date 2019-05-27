ae3c2babbdd430ebd815ef9673ccf1daf139aa5d
Xelaflash <xelaflash@gmail.com>
2019-05-27 18:37:42 -0400

    carousel with indicators working

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,6 +1,7 @@
-import 'jquery';
+import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
+
 // components JS files
 import { openNav, closeNav } from './components/mobile_nav';
 
@@ -9,3 +10,7 @@ const closeBtn = document.querySelector(".closebtn");
 
 navBtn.addEventListener("click", openNav);
 closeBtn.addEventListener("click", closeNav);
+
+$('.carousel').carousel({
+  interval: 9000
+});
