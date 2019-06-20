da9f57d612bdb953df0a5fc7705273050c27bb39
Xelaflash <xelaflash@gmail.com>
2019-06-20 13:37:06 -0400

    add yarn flatpickr

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,12 +1,14 @@
 // import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
-
+import flatpickr from "flatpickr";
 
 // components JS files
 import { openNav, closeNav } from './components/mobile_nav';
 import './components/nav_color';
 import './components/form-validation';
+
+flatpickr(".datepicker", {});
 
 const navBtn = document.querySelector(".burger");
 const closeBtn = document.querySelector(".closebtn");
