e47053f8e309f80aba142815e40d8d50045ee39d
Xelaflash <xelaflash@gmail.com>
2019-06-21 12:07:31 -0400

    init intl tel nbr

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -2,21 +2,29 @@
 import 'popper.js';
 import 'bootstrap';
 import flatpickr from "flatpickr";
+import intlTelInput from 'intl-tel-input';
 
 // components JS files
 import { openNav, closeNav } from './components/mobile_nav';
 import './components/nav_color';
 import './components/form-validation';
 
+
+// nav mobile
+const navBtn = document.querySelector(".burger");
+const closeBtn = document.querySelector(".closebtn");
+navBtn.addEventListener("click", openNav);
+closeBtn.addEventListener("click", closeNav);
+
+
+// date picker init
 flatpickr(".datepicker", {
   dateFormat: "d/m/Y"
 });
 
-const navBtn = document.querySelector(".burger");
-const closeBtn = document.querySelector(".closebtn");
-
-navBtn.addEventListener("click", openNav);
-closeBtn.addEventListener("click", closeNav);
+// tel input init
+const input = document.querySelector("#phone");
+intlTelInput(input);
 
 // carousel js
 // $('.carousel').carousel({
