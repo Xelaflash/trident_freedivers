005274cdb69f79a6a34e87d6f87c90a4baecc69a
Xelaflash <xelaflash@gmail.com>
2019-06-25 14:09:54 -0400

    back to top done hiiden xs

---
+++ source/assets/javascripts/components/back_to_top.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+const btn = $('.back_to_top');
+
+$(window).scroll(() => {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+$(document).ready(() => {
+  $(".back_to_top").click((event) => {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+    return false;
+  });
+});
