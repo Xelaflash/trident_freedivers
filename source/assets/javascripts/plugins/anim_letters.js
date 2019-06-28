4c2cf5b2be8c361d564aebb1910520146d89e717
Xelaflash <xelaflash@gmail.com>
2019-06-28 14:11:59 -0400

    reput letter anim

--- source/assets/javascripts/plugins/anim_letters.js
+++ source/assets/javascripts/plugins/anim_letters.js
@@ -1,56 +1,56 @@
-// // import $ from 'jquery';
-// import anime from 'animejs';
+// import $ from 'jquery';
+import anime from 'animejs';
 
-// const sUsrAg = navigator.userAgent;
-// let sBrowser;
-// // The order matters here, and this may report false positives for unlisted browsers.
-// if (sUsrAg.indexOf("Firefox") > -1) {
-//   sBrowser = "Mozilla Firefox";
-// } else if (sUsrAg.indexOf("Opera") > -1 || sUsrAg.indexOf("OPR") > -1) {
-//   sBrowser = "Opera";
-// } else if (sUsrAg.indexOf("Trident") > -1) {
-//   sBrowser = "Microsoft Internet Explorer";
-// } else if (sUsrAg.indexOf("Edge") > -1) {
-//   sBrowser = "Microsoft Edge";
-// } else if (sUsrAg.indexOf("Chrome") > -1) {
-//   sBrowser = "Google Chrome or Chromium";
-// } else if (sUsrAg.indexOf("Safari") > -1) {
-//   sBrowser = "Apple Safari";
-// } else {
-//   sBrowser = "unknown";
-// }
+const sUsrAg = navigator.userAgent;
+let sBrowser;
+// The order matters here, and this may report false positives for unlisted browsers.
+if (sUsrAg.indexOf("Firefox") > -1) {
+  sBrowser = "Mozilla Firefox";
+} else if (sUsrAg.indexOf("Opera") > -1 || sUsrAg.indexOf("OPR") > -1) {
+  sBrowser = "Opera";
+} else if (sUsrAg.indexOf("Trident") > -1) {
+  sBrowser = "Microsoft Internet Explorer";
+} else if (sUsrAg.indexOf("Edge") > -1) {
+  sBrowser = "Microsoft Edge";
+} else if (sUsrAg.indexOf("Chrome") > -1) {
+  sBrowser = "Google Chrome or Chromium";
+} else if (sUsrAg.indexOf("Safari") > -1) {
+  sBrowser = "Apple Safari";
+} else {
+  sBrowser = "unknown";
+}
 
-// function animeLetters() {
-//   // eslint-disable-next-line func-names
-//   if (sBrowser !== "Apple Safari") {
-//     // eslint-disable-next-line func-names
-//     $('.ml2').each(function () {
-//       // eslint-disable-next-line no-control-regex
-//       $(this).html($(this).text().replace(/([^\x00-\x80]|\S)/g, "<span class='letter'>$&</span>"));
-//     });
-//     anime.timeline({
-//       loop: 1
-//     }).add({
-//       targets: '.ml2 .letter',
-//       scale: [4, 1],
-//       opacity: [0, 1],
-//       translateZ: 0,
-//       easing: "easeOutExpo",
-//       duration: 1200,
-//       delay(_el, i) {
-//         return 150 * i;
-//       }
-//     }).add({
-//       targets: '.ml2',
-//       opacity: 1,
-//       duration: 1000,
-//       easing: "easeOutExpo",
-//       delay: 1000,
-//       loop: 1
-//     });
-//   }
-// }
+function animeLetters() {
+  // eslint-disable-next-line func-names
+  if (sBrowser !== "Apple Safari") {
+    // eslint-disable-next-line func-names
+    $('.ml2').each(function () {
+      // eslint-disable-next-line no-control-regex
+      $(this).html($(this).text().replace(/([^\x00-\x80]|\S)/g, "<span class='letter'>$&</span>"));
+    });
+    anime.timeline({
+      loop: 1
+    }).add({
+      targets: '.ml2 .letter',
+      scale: [4, 1],
+      opacity: [0, 1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 1200,
+      delay(_el, i) {
+        return 150 * i;
+      }
+    }).add({
+      targets: '.ml2',
+      opacity: 1,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000,
+      loop: 1
+    });
+  }
+}
 
-// window.addEventListener('DOMContentLoaded', () => {
-//   window.setTimeout(animeLetters, 1000);
-// });
+window.addEventListener('DOMContentLoaded', () => {
+  window.setTimeout(animeLetters, 1000);
+});
