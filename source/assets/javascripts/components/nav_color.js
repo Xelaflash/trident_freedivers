20b644882e722ee162331256529630c43b3fb464
Xelaflash <xelaflash@gmail.com>
2019-08-13 11:38:47 -0400

    re-add html extension for nav color

--- source/assets/javascripts/components/nav_color.js
+++ source/assets/javascripts/components/nav_color.js
@@ -1,8 +1,8 @@
 const nav = document.getElementById("navbar");
 const currentPage = document.location.pathname;
 
-if (currentPage === "/spearfishing/") {
+if (currentPage === "/spearfishing.html") {
   nav.style.backgroundColor = "rgba(28, 136, 179, 0.8)";
-} else if (currentPage === "/contact/") {
+} else if (currentPage === "/contact.html") {
   nav.style.backgroundColor = "transparent";
 }
