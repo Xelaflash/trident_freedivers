96f9c26215545f238ab8bf377f4fbf4afca62bdb
Xelaflash <xelaflash@gmail.com>
2019-10-22 12:34:24 -0400

    small adjustment css (opacity nav for spear_page + card_tours smaller small screen

--- source/assets/javascripts/components/nav_color.js
+++ source/assets/javascripts/components/nav_color.js
@@ -2,7 +2,7 @@ const nav = document.getElementById("navbar");
 const currentPage = document.location.pathname;
 
 if (currentPage === "/spearfishing.html") {
-  nav.style.backgroundColor = "rgba(28, 136, 179, 0.8)";
+  nav.style.backgroundColor = "rgba(28, 136, 179, 0.7)";
 } else if (currentPage === "/contact.html") {
   nav.style.backgroundColor = "transparent";
 }
