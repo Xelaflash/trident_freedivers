5cfc62a0b42a1971d8a4e624b138611f5c566d0c
Alexandre Gros <xelaflash@gmail.com>
2023-12-04 11:59:14 -0500

    remove logs

--- source/assets/javascripts/components/nav_color.js
+++ source/assets/javascripts/components/nav_color.js
@@ -1,11 +1,8 @@
 const nav = document.getElementById("navbar");
-console.log(nav);
 const currentPage = document.location.pathname;
-console.log(currentPage);
 
 if (currentPage === "/spearfishing") {
   nav.style.backgroundColor = "rgba(28, 136, 179, 0.7)";
-  console.log('here');
 } else if (currentPage === "/contact") {
   nav.style.backgroundColor = "transparent";
 }
